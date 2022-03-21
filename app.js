f826ae55b74e4836c540a9081707e51b791f3baf
azhim <ariefazhim@gmail.com>
2022-03-21 15:12:28 +0800

    added content for app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector(".sliderWrapper");
+/* querySelector() returns the first Element within the document 
+that matches the specified selector,
+or group of selectors. If no matches are found, null is returned.*/
+
+console.log(wrapper);
