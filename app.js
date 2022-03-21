8696e62ee92e22434f3145c3777de6199504e091
azhim <ariefazhim@gmail.com>
2022-03-21 15:20:32 +0800

    added style in app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const wrapper = document.querySelector(".sliderWrapper");
 /* querySelector() returns the first Element within the document 
 that matches the specified selector,
-or group of selectors. If no matches are found, null is returned.*/
+or group of selectors. If no matches are found, null is returned.
+*/
+const menuItems = document.querySelectorAll(".menuItem")
 
 console.log(wrapper);
+
+wrapper.style.transform = "translateX(-100vw)"
+/*
+for movement of slider 
+*/
