2672835f226a8bc060db26c8f8627af4eced5803
azhim <ariefazhim@gmail.com>
2022-03-22 10:54:44 +0800

    finished slider

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const wrapper = document.querySelector(".sliderWrapper");
 that matches the specified selector,
 or group of selectors. If no matches are found, null is returned.
 */
-const menuItems = document.querySelectorAll(".menuItem")
+const menuItems = document.querySelectorAll(".menuItem");
 
-console.log(wrapper);
-
-wrapper.style.transform = "translateX(-100vw)"
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    wrapper.style.transform = `translateX(${-100 * index}vw )`;
+  });
+});
 /*
+Above is an event listener for the slider
+wrapper.style.transform = "translateX(-100vw)"
 for movement of slider 
 */
